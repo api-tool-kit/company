bf68d96a05ca40fea10f0f237bbb39044abac645
Mike Amundsen <mamund@yahoo.com>
2020-01-20 20:00:44 -0500

    update lib

--- darrt/lib/component.js
+++ darrt/lib/component.js
@@ -32,7 +32,9 @@ function main(args) {
   item = args.item||{};
   reqd = args.reqd||[];
   enums = args.enums||[];
- 
+  defs = args.defs||[];
+  fields = args.fields||"";
+  
   // confirm existence of object storage
   storage({action:'create',object:elm});
 
@@ -48,17 +50,17 @@ function main(args) {
       rtn = profile;
       break;
     case 'list':
-      rtn = utils.cleanList(storage({object:elm, action:'list'}));
+      rtn = utils.cleanList(storage({object:elm, action:'list', fields:fields}));
       break;
     case 'read':
     case 'item':
-      rtn = utils.cleanList(storage({object:elm, action:'item', id:id}));
+      rtn = utils.cleanList(storage({object:elm, action:'item', id:id, fields:fields}));
       break;
     case 'filter':
-      rtn = utils.cleanList(storage({object:elm, action:'filter', filter:filter}));
+      rtn = utils.cleanList(storage({object:elm, action:'filter', filter:filter, fields:fields}));
       break
     case 'add':
-      rtn = addEntry(elm, item, props, reqd, enums);
+      rtn = addEntry(elm, item, props, reqd, enums, defs);
       break;
     case 'update':
       rtn = updateEntry(elm, id, item, props, reqd, enums);
--- darrt/lib/storage.js
+++ darrt/lib/storage.js
@@ -32,19 +32,20 @@ function main(args) {
   var filter = args.filter||null;
   var id = args.id||null;
   var item = args.item||{};
+  var fields = args.fields||"";
 
   switch (action) {
     case 'create':
       rtn = createObject(object);
       break;
     case 'list':
-      rtn = getList(object);
+      rtn = getList(object, null, fields);
       break;
     case 'filter':
-      rtn = getList(object, filter);
+      rtn = getList(object, filter, fields);
       break;
     case 'item':
-      rtn = getItem(object, id);
+      rtn = getItem(object, id, fields);
       break;
     case 'add':
       rtn = addItem(object, item, id);
@@ -65,7 +66,7 @@ function main(args) {
 }
 
 // get a list of items (possibly via filter)
-function getList(object, filter) {
+function getList(object, filter, fields) {
   var coll, item, list, i, x, t, name;
 
   coll = [];
@@ -99,11 +100,16 @@ function getList(object, filter) {
     coll = [];
   }
 
+  // apply field filter
+  for(i=0,x=coll.length;i<x;i++) {
+    coll[i] = applyFields(coll[i],fields);
+  }
+
   return coll;
 }
 
 // retrieve and existing item
-function getItem(object, id) {
+function getItem(object, id, fields) {
   var rtn;
 
   try {
@@ -112,7 +118,30 @@ function getItem(object, id) {
     rtn = exception("SimpleStorage: ["+object+"]", ex.message, 400);
   }
 
+  rtn = applyFields(rtn, fields);
+
   return rtn;
+}
+
+// apply field list
+// item = object to return
+// fields = a string of field names to return
+function applyFields(item,fields) {
+  var rtn = {};
+  
+  if(fields.length!==0) {
+    for(var i in item) {
+      if(fields.indexOf(i)!==-1) {
+        rtn[i] = item[i];
+      }
+    }
+  }
+  else {
+    rtn = item;
+  }
+  
+  return rtn;
+  
 }
 
 // create a storage object (folder)
@@ -212,8 +241,8 @@ function exception(name, message, code, type, url) {
 
   rtn.type = (type||"error");
   rtn.title = (name||"Error");
-  rtn.detail = (message||rtn.name);
-  rtn.status = (code||400);
+  rtn.detail = (message||name);
+  rtn.status = (code||"400");
   if(url) {rtn.instance = url};
 
   return rtn;
