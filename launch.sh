253ae59c79a8e9b70331ea541913886d80477749
Mike Amundsen <mamund@yahoo.com>
2020-02-27 20:01:38 -0500

    update scripts

--- launch.sh
+++ launch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# **************************************
+# **********************************************
 # launch : launch-test-deploy
 #
 # see "Design and Build Great Web APIs"
 # 2020-04 : @mamund
 # 
 # notes:
-#   - set up "npm run dev" in package.json
-#   - create test collection in postman (test-run.sh)
-#   - establish heroku app using git
+#   - set up "npm run dev" in package.json (uses nodemon)
+#   - create test collection in postman (calleld in test-run.sh)
+#   - establish heroku app using git (via heroku cli)
 #
 # assumes:
-#   - you have npm and package mgmt
+#   - you have npm, package mgmt, and nodemon
 #   - you have postman collections
 #   - you have a heroku git remote
 #
@@ -20,34 +20,34 @@
 #   - npm
 #   - git
 #
-# **************************************
+# **********************************************
 
-# **************************************
+# **********************************************
 # exit var
 ex=0
 
 # commands
 svc="npm run dev"
 test_pre="./test-run.sh local"
-deploy="git push heroku master"
 test_post="./test-run.sh remote"
+deploy="git push heroku master"
 
-# **************************************
+# **********************************************
 # setup killing backgrounds when done
 trap "kill 0" EXIT 
 
-# **************************************
-# start local instance of service
+# **********************************************
+# start local instance of service as background
 $svc &
 
-# **************************************
+# **********************************************
 # run pre-deploy test script
 echo "Running pre-deployment tests..."
 cd tests/postman
 $test_pre
 ex=$?
 
-# **************************************
+# **********************************************
 # check test status
 if [ $ex -eq 1 ]
 then
@@ -56,13 +56,13 @@ then
   exit $ex
 fi  
 
-# **************************************
+# **********************************************
 # use git push to heroku
 echo "Deploying to production..."
 $deploy
 ex=$?
 
-# **************************************
+# **********************************************
 # check heroku status
 if [ $ex -eq 1 ]
 then
@@ -71,14 +71,14 @@ then
   exit $ex
 fi  
 
-# **************************************
+# **********************************************
 # run post-deploy test script
 echo "Running post-deployment tests..."
 cd tests/postman
 $test_post
 ex=$?
 
-# **************************************
+# **********************************************
 # check test status
 if [ $ex -eq 1 ]
 then
@@ -87,7 +87,7 @@ then
   exit $ex
 fi  
 
-# **************************************
+# **********************************************
 # report successful run
 echo 
 echo "*****************************"
@@ -96,7 +96,7 @@ echo "*****************************"
 date
 exit 0
 
-# **************************************
+# **********************************************
 # EOF
-# **************************************
+# **********************************************
 
